9ec93cc5152ec78ca90382ed70c3a5976caadcad
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-06-17 18:40:13 +0700

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -8,33 +8,3 @@ export default (props) => {
     }
   });
 }
-
-/*
-const props = {
-  projectRoot: '',
-  staticConfigPath: 'app.json',
-  packageJsonPath: 'package.json',
-  // from app.json->expo
-  config: {
-    name: 'My App',
-    version: '0.0.0',
-    slug: 'myapp',
-    scheme: 'myapp',
-    platforms: [ 'ios', 'android', 'web' ],
-    orientation: 'portrait',
-    icon: './assets/images/icon.png',
-    splash: {
-      image: './assets/images/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#ffffff'
-    },
-    updates: { fallbackToCacheTimeout: 0 },
-    assetBundlePatterns: [ '**\/*' ],
-    ios: { supportsTablet: true },
-    web: { favicon: './assets/images/favicon.png' },
-    extra: {}, // from app.json->expo.extra
-    description: 'Description', // from package.json->description
-    sdkVersion: '37.0.0',  // from package.json
-  }
-};
-*/
